c1af4132cab04b1b028bef9bb519e3c9b040b477
zunzum <zunzum@naver.com>
2022-10-03 21:49:29 +0900

    Initial commit

---
+++ app/src/main/java/com/army/myfirstandroidapp_zun/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.army.myfirstandroidapp_zun
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
